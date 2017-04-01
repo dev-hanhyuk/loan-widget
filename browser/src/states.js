f2400f2bf37b4443c9b9e74c9c90dbd19f9cfce2
Han Hyuk Cho <han@Hans-MacBook-Air.local>
2017-04-01 13:19:41 -0400

    updated font and screen size

--- browser/src/states.js
+++ browser/src/states.js
@@ -6,7 +6,6 @@ app.config(($stateProvider) =>
     views: {
       '': { templateUrl: 'src/templates/main.template.html', conroller: 'MainCtrl'},
       'bg@main': {templateUrl: 'src/templates/bg.template.html'},
-      'intro@main': { templateUrl: 'src/templates/intro.template.html' },
       'form@main': {templateUrl: 'src/templates/form.template.html', controller: 'MainCtrl'},
       'footer@main': { templateUrl: 'src/templates/footer.template.html' }
     }
