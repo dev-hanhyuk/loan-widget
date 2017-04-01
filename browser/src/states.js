eab92f08a354f83faef02e6682f863dd7d8baf0d
Han Hyuk Cho <han@Hans-MacBook-Air.local>
2017-03-31 20:24:28 -0400

    bg and parallax

--- browser/src/states.js
+++ browser/src/states.js
@@ -5,6 +5,8 @@ app.config(($stateProvider) =>
     url: '/main',
     views: {
       '': { templateUrl: 'src/templates/main.template.html', conroller: 'MainCtrl'},
+      'bg@main': {templateUrl: 'src/templates/bg.template.html'},
+      'intro@main': { templateUrl: 'src/templates/intro.template.html' },
       'form@main': {templateUrl: 'src/templates/form.template.html', controller: 'MainCtrl'},
       'footer@main': { templateUrl: 'src/templates/footer.template.html' }
     }
